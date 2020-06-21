209e017be44f93e4c9eb3d949c3a06ec0be02903
asadullahbappi <58213620+asadullahbappi@users.noreply.github.com>
2020-06-21 01:08:35 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> by asad.
+          Edit <code>src/App.js</code> by Asadullah Bappi.
         </p>
         <a
           className="App-link"
